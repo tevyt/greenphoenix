0c3225a9c519e784a4dfe37fed045ca6ee03eeae
Travis Smith <travissmith@pop-os.localdomain>
2019-10-02 18:47:12 -0500

    feature: Creates first row of greeting section.
    The first row of the greeting section consists of two links. One, a link to the home page and the send a link to a page to leave a message.

--- src/sections/greeting/greeting-view.js
+++ src/sections/greeting/greeting-view.js
@@ -1,10 +1,41 @@
 import React from "react"
+import { graphql, useStaticQuery, Link } from "gatsby"
 
 import "./greeting-styles.scss"
 
 function Greeting() {
+  const {
+    file: {
+      childImageSharp: {
+        fixed: { src },
+      },
+    },
+  } = useStaticQuery(graphql`
+    query GetSymbol {
+      file(relativePath: { eq: "symbol.png" }) {
+        childImageSharp {
+          fixed(width: 54, height: 105) {
+            src
+          }
+        }
+      }
+    }
+  `)
   return (
-    <section className="section greeting hero is-fullheight is-black"></section>
+    <section className="section greeting hero is-fullheight is-black">
+      <div className="hero-head columns greeting-head">
+        <div className="column greeting-head-logo">
+          <Link to="/test">
+            <img src={src} alt="home link" />
+          </Link>
+        </div>
+        <div className="column greeting-head-link">
+          <Link className="button is-outlined is-rounded" to="/message">
+            Message
+          </Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
