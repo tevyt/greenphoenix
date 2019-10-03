82d90046be8f238440d75772d612ee2244a6035f
Travis Smith <travissmith@pop-os.localdomain>
2019-10-02 20:25:15 -0500

    feature: Adds icons for familiar technologies.
    A row has been added to display familiar technologies. To accomplish this fontawesome has been added. The icons used come exclusively from fontawesome-free-brands.

--- src/sections/greeting/greeting-view.js
+++ src/sections/greeting/greeting-view.js
@@ -1,5 +1,16 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faJava,
+  faAndroid,
+  faPython,
+  faAngular,
+  faReact,
+  faNodeJs,
+  faAws,
+  faGitAlt,
+} from "@fortawesome/free-brands-svg-icons"
 
 import "./greeting-styles.scss"
 
@@ -43,6 +54,34 @@ function Greeting() {
             src="http://placehold.it/200"
             alt="An green outline of a phoenix"
           />
+        </div>
+      </div>
+      <div className="hero-footer">
+        <div className="container greeting-footer">
+          <span className="icon">
+            <FontAwesomeIcon icon={faJava} size="3x" />
+          </span>
+          <span className="icon">
+            <FontAwesomeIcon icon={faGitAlt} size="3x" />
+          </span>
+          <span className="icon">
+            <FontAwesomeIcon icon={faAndroid} size="3x" />
+          </span>
+          <span className="icon">
+            <FontAwesomeIcon icon={faPython} size="3x" />
+          </span>
+          <span className="icon">
+            <FontAwesomeIcon icon={faAngular} size="3x" />
+          </span>
+          <span className="icon">
+            <FontAwesomeIcon icon={faReact} size="3x" />
+          </span>
+          <span className="icon">
+            <FontAwesomeIcon icon={faNodeJs} size="3x" />
+          </span>
+          <span className="icon">
+            <FontAwesomeIcon icon={faAws} size="3x" />
+          </span>
         </div>
       </div>
     </section>
