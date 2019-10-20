3683c2e9876cae627d4b6d3f8907e946fe3f2b7a
Travis Smith <travis.a.smith.ja@gmail.com>
2019-10-20 18:23:27 -0500

    feature: Creates outline of the greetings section.
    Creates a greetings section that introduces me and displays a container
    for my skills.

--- src/sections/introduction/introduction-view.js
+++ src/sections/introduction/introduction-view.js
@@ -6,8 +6,20 @@ function Introduction() {
   return (
     <section className="introduction section">
       <div className="introduction-container">
-        <div className="introduction-container-name">Placeholder</div>
-        <p className="introduction-container-summary">who who who who</p>
+        <div className="introduction-container-name">
+          I'm Travis, a Software Developer.
+        </div>
+        <p className="introduction-container-summary">
+          Over the past 3 years I have had the opportunity to work in a rapidly
+          changing environment. During this time, I have cultivated skills that
+          are essential for any organization seeking digital transformation.
+        </p>
+      </div>
+      <div className="introduction-skills">
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
+        <div>test</div>
       </div>
     </section>
   )
