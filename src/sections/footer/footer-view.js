942e4d9d383e60888b63baa84caefa85b41affd4
Travis Smith <travis.a.smith.ja@gmail.com>
2019-10-21 20:35:58 -0500

    feature: Creates footer section.

--- src/sections/footer/footer-view.js
+++ src/sections/footer/footer-view.js
@@ -1,15 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import "./footer-styles.scss"
+import ButtonLink from "../../components/ButtonLink"
+
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer-greeting">Want to chat?</div>
-      <div className="footer-message">
-        You can leave a message here. Let's work together sometime.
-      </div>
-      <div className="footer-button">
-        <Link to="/message">Leave a Message</Link>
+    <div className="site-footer">
+      <div className="site-footer-container">
+        <div className="site-footer-greeting">Want to chat?</div>
+        <div className="site-footer-message">
+          You can leave a message here. Let's work together sometime.
+        </div>
+        <div className="site-footer-link">
+          <ButtonLink to="/message" label="Leave a message" />
+        </div>
       </div>
     </div>
   )
