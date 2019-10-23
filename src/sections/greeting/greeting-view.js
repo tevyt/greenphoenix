fe81a81e4fa4c70bf2b008787cde353004164314
Travis Smith <travis.a.smith.ja@gmail.com>
2019-10-22 22:11:26 -0500

    feature: Adds github and linkedin links.

--- src/sections/greeting/greeting-view.js
+++ src/sections/greeting/greeting-view.js
@@ -5,13 +5,23 @@ import HomeLink from "../../components/HomeLink"
 import ButtonLink from "../../components/ButtonLink"
 import LargeLogo from "../../components/LargeLogo/large-logo-view"
 import ToolBanner from "../../components/ToolBanner"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
 function Greeting() {
   return (
     <section className="section greeting hero is-fullheight is-black">
       <div className="hero-head greeting-head">
         <HomeLink />
-        <ButtonLink to="/message" label="Message" />
+        <div className="greeting-head-contact">
+          <a href="https://github.com/tevyt">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://www.linkedin.com/in/travisalexandersmith/">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <ButtonLink to="/message" label="Message" />
+        </div>
       </div>
       <div className="hero-body">
         <div className="container has-text-centered greeting-body">
