545ea63f3be5ee9692d4a27aa0b29a4d8b3dc648
Travis Smith <travis.a.smith.ja@gmail.com>
2019-10-22 22:27:23 -0500

    bug: Adds exception handling to useWindowWidth hook.
    During server side rendering the window value is not available.
    Therefore during Gatsby's build process. A check had to be added to
    prevent the error for the missing window from stopping the build.

--- src/hooks/useWindowWidth.js
+++ src/hooks/useWindowWidth.js
@@ -1,18 +1,24 @@
 import React from "react"
 
 function useWindowWidth() {
-  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
-  React.useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth)
-    }
-    window.addEventListener("resize", handleResize)
-    return () => {
-      window.removeEventListener("resize", handleResize)
-    }
-  })
+  try {
+    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+    React.useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth)
+      }
+      window.addEventListener("resize", handleResize)
+      return () => {
+        window.removeEventListener("resize", handleResize)
+      }
+    })
 
-  return windowWidth
+    return windowWidth
+  } catch (e) {
+    //window is not available during server side rendering.
+    console.warn(e)
+    return 0
+  }
 }
 
 export default useWindowWidth
