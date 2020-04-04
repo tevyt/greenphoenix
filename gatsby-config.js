80d81245172d924a10e071d842a14300a77c64d4
Travis Smith <travis.a.smith.ja@gmail.com>
2020-04-04 14:21:55 -0500

    ops: Sets up preview environment for contentful.
    A new configuration for host has been added to the gatsby config to
    allow for the contentful preview host to be used in development. While
    `cdn.contentful.com` is set when building for production.

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,7 @@ module.exports = {
       options: {
         spaceId: process.env.GATSBY_PORTFOLIO_CONTENTFUL_SPACE_ID,
         accessToken: process.env.GATSBY_PORTFOLIO_CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.GATSBY_PORTFOLIO_CONTENTFUL_HOST
       },
     },
   ],
